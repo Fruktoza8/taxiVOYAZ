6a99342caad1ea2d9a8770526b6942f6e1fb72d5
Orlenko Vladislav <79062398+Fruktoza8@users.noreply.github.com>
2023-12-06 16:11:30 +0300

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$dbname = 'taxi';
+$username = 'root';
+$password = '';
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Установка режима обработки исключений
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Ошибка подключения к базе данных: " . $e->getMessage();
+    exit;
+}
+?>
