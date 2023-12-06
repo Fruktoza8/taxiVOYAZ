686de6ef705e315e2a7a8acedf04577f9ffe4043
Orlenko Vladislav <79062398+Fruktoza8@users.noreply.github.com>
2023-12-06 16:12:36 +0300

    Create index.php

---
+++ index/index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Главная Страница</title>
+    <script src="https://api-maps.yandex.ru/2.1/?apikey=fc9fa09e-35e9-4738-a2d2-e5ec86c7c4ab&lang=ru_RU" type="text/javascript"></script>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<div id="map"></div>
+<script src="index.js" type="text/javascript"></script>
+</body>
+</html>
+
